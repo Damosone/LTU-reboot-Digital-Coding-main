e89f2423d07da4feca6d72517cb6c3c598176963
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-05 20:59:31 +0000

    Update task3.1.4.js

---
+++ scripts/task3.1.4.js
@@ -0,0 +1,11 @@
+console.log ("Hello from the console");
+
+alert("hello from alert pop up");
+
+document.write("hello from the doc write");
+
+ var yearIWasborn =1985;
+ var currentYear = 2020;
+ var myAge= (currentYear - yearIWasborn)
+ 
+  console.log (myAgeIs);
