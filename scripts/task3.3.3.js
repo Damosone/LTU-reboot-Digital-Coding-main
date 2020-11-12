197c89140feed34c5e95304ff67e96a6918f04ce
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-12 21:01:41 +0000

    Update task3.3.3.js

--- scripts/task3.3.3.js
+++ scripts/task3.3.3.js
@@ -1,19 +1,29 @@
 
 function calculator ( number1, number2, operator){
 
+ var result = ' ';
 
-switch ( operator ) {
-    case '+' :
-        
-        console.log( number1 + number2);
+
+    switch ( operator ) {
+        case '+' :
+            result = number1 +number2;
+            console.log( number1 +  ' +' + number2 + ' = ' + result );
 
         break ;
+        case '-' :
+        
+            console.log( number1 - number2);
+    
+        break ;
 
-    default :
+        default :
+            console.log("Math is not right here");
+            
             // error message
 
 
         break;
-}
+    }
 
 }
+//+ addition - subtraction / division *multiply modulus
