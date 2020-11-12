2a23f43e9c39aaefde0c84dfe4ffbeb209d91180
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-12 20:53:09 +0000

    Create task3.3.3.js

---
+++ scripts/task3.3.3.js
@@ -0,0 +1,19 @@
+
+function calculator ( number1, number2, operator){
+
+
+switch ( operator ) {
+    case '+' :
+        
+        console.log( number1 + number2);
+
+        break ;
+
+    default :
+            // error message
+
+
+        break;
+}
+
+}
