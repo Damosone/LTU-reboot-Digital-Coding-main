d69db55ec61117adc1ac13391e2eb6a09c2223e9
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-12 19:29:28 +0000

    Update task3.2.6.js

---
+++ scripts/task3.2.6.js
@@ -0,0 +1,31 @@
+var temperature = 25;
+
+
+if (temperature < 0){
+
+    console.log('Stay Inside.');
+
+}  else if (temperature < 30){
+
+    console.log('Wear a coat and hat.');
+
+}
+if (temperature < 50){
+
+    console.log('Wear a coat.');
+
+}
+else if (temperature > 50 && temperature == 55) {
+
+    console.log('its hot in here');
+
+}
+
+else if (temperature > 50 ||  temperature === 55) {
+
+    console.log('sombody gone get burn');
+}
+
+// adding logical references
+
+
