e25fc4476755a777902025274da7135690579bb1
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-12 20:36:07 +0000

    Create task3.3.2.js

---
+++ scripts/task3.3.2.js
@@ -0,0 +1,30 @@
+
+function drinkOrder( drink, size ) {
+
+    var drinkToLowerCase = drink.ToLowercase();
+
+    switch( drinkToLowerCase ) {
+        case 'cola' :
+         console.log("You have ordered a "+ size + " " + rinkToLowerCase );
+
+         break;
+
+         case 'lemonade' :
+         console.log("You have ordered a "+ size + " " + rinkToLowerCase );
+
+         break;
+
+         case 'orangeade' :
+         console.log("You have ordered a "+ size + " " + drrinkToLowerCase );
+
+         break;
+
+
+         default:
+            console.log("The drink which you tried to order in not on Menu. We are sorry ");
+
+            break; 
+    
+    }
+
+}
