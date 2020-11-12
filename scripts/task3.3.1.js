f90a5c7a6a5198ac869ea25f291f31411b3c9b5d
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-12 19:20:08 +0000

    Create task3.3.1.js

---
+++ scripts/task3.3.1.js
@@ -0,0 +1,22 @@
+var temperature = 25;
+
+
+if (temperature < 0){
+
+    console.log('Stay Inside.');
+
+}  else if (temperature < 30){
+
+    console.log('Wear a coat and hat.');
+
+}
+if (temperature < 50){
+
+    console.log('Wear a coat.');
+
+}
+else{
+
+    console.log('Just pants and vest is fine.');
+
+}
