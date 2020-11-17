79052e2f1b0f450dcb0ae7dd861cd904a4896fa6
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-17 20:17:35 +0000

    Create task3.4.2.js

---
+++ scripts/task3.4.2.js
@@ -0,0 +1,9 @@
+var favouriteFoods = ["pizza", "ice-cream", "kebab", "chips"];
+
+
+for( var index = 0; index < favouriteFoods.length; index++) {
+
+    console.log( favouriteFoods[index] );
+}
+
+console.log
