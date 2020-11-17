dfa1f95e0b6bcc2b13576492acebc773cfd70eb7
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-17 20:42:33 +0000

    Create task3.4.3.js

---
+++ scripts/task3.4.3.js
@@ -0,0 +1,11 @@
+var favouriteFoods = ["apple crumble and custard", "ice-cream", "chocolate", "cream soda"];
+
+
+for( var index = 0; index < favouriteFoods.length; index++) {
+
+    console.log( favouriteFoods[index] );
+
+    document.write( favouriteFoods + "<br>")
+}
+
+console.log(favouriteFoods[1]);
