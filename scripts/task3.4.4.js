4af0328165f017fd0c32aea12081539c10c7e6af
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-19 19:07:54 +0000

    Update task3.4.4.js

--- scripts/task3.4.4.js
+++ scripts/task3.4.4.js
@@ -1,3 +1,21 @@
 var spaghetti = {
-recipeTitle = "how to make a spaghetti"
-}
+
+    recipeTitle : "how to make a spaghetti",
+
+    Servings: 2,
+    
+    Ingredients : [" cann of chopped tomatos", "mushrooms", "onions", "bell peppers", "mince meat", "herbs"],
+
+    step1: ["Put a large saucepan on a medium heat and add 1 tbsp olive oil"],
+
+    step2: ["Add finely chopped onions, mushrooms,  peppers and fry for a 10 min"],
+
+    step3: ["Add mince meat and fry for another 10 min or until done"],
+
+    step4: ["add can of chopped tomatos "],
+
+    Directions: [step1 + "<br>" + step2 + "<br>" + step3 + "<br>" + step4 + "Enjoy :)"],
+
+    
+
+}     
