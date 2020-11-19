2f6339405dcc8295e3d9faddaa38c935a4a7d213
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-19 19:26:51 +0000

    Update task3.4.4.js

--- scripts/task3.4.4.js
+++ scripts/task3.4.4.js
@@ -1,10 +1,10 @@
 var spaghetti = {
 
-    recipeTitle : "how to make a spaghetti",
+    recipeTitle : " spaghetti",
 
     Servings: 2,
     
-    Ingredients : [" cann of chopped tomatos", "mushrooms", "onions", "bell peppers", "mince meat", "herbs"],
+    Ingredients : [" can of chopped tomatos", "mushrooms", "onions", "bell peppers", "mince meat", "herbs"],
 
     step1: ["Put a large saucepan on a medium heat and add 1 tbsp olive oil"],
 
@@ -16,6 +16,9 @@ var spaghetti = {
 
     Directions: [step1 + "<br>" + step2 + "<br>" + step3 + "<br>" + step4 + "Enjoy :)"],
 
-    
+    console.log("Lets cook my favourite recipe"+ recipeTitle + " " + "this are things we need" ),
 
 }     
+
+// code pen link from Luke : https://codepen.io/pixel_to_code/pen/qBZMMOg?editors=0012
+
