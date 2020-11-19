9e244a98bdba75508de87d18d48f6fcdd5bec537
Damosone <72577509+Damosone@users.noreply.github.com>
2020-11-19 20:34:16 +0000

    Update task3.5.2.js

--- scripts/task3.5.2.js
+++ scripts/task3.5.2.js
@@ -61,21 +61,50 @@ let shoppingCart = [
   }];
   
   
-  function shoppingCartTotalPrice( arr ) { // 1. Create a function that takes 1 argument (the array)
-    
-    let totalPrice = 0; // 2. Create a variable inside the array called “totalPrice”
-    
-    // 3. Loop through each item in the array and add the value of the item to the total price, remember to account for the quantity.
-    for ( let index = 0; index < arr.length; index++ ) { 
-    
-      totalPrice = totalPrice + ( arr[ index ].price * arr[ index ].quantity );
-   
-    }
-    
-    // 4. Return the totalPrice Variable
-    return totalPrice.toFixed( 2 );
+  //function shoppingCartTotalPrice( arr ) { // 1. Create a function that takes 1 argument (the array)
+    function discoutForFood (arr){
+        var totalPrice = 0;
+
+        for( var index = 0; index < arr.length; index++){
+
+            //step 4. of the revieew with Luke - adding discount 
+                if ( arr[index].type === "food"){
+
+                    var discunt = ( arr[index].price *20) / 100;
+
+                    totalPrice = totalPrice + (arr[index].price - discount ) * arr[index].quantity;
+
+                } else {
+                    
+                    totalPrice = totalPrice + ( arr[index].price * arr[index].quantity);
+                }
+
+                return totalPrice.toFixed( 2 );
     
   }
   
-  document.write( '<div class="total-cost alert alert-primary" role="alert">£ ' + shoppingCartTotalPrice( shoppingCart ) + '</div>') ; // 5. Console.log the returned value -  I replaced with document.write()
+  document.write( '<div class="total-cost alert alert-primary" role="alert">£ ' + shoppingCartTotalPrice( shoppingCart ) + '</div>');
+
+        }
+
+
+    
+    //let totalPrice = 0; // 2. Create a variable inside the array called “totalPrice”
+    
+    // 3. Loop through each item in the array and add the value of the item to the total price, remember to account for the quantity.
+    //for ( let index = 0; index < arr.length; index++ ) { 
+    
+      
+     // if ( arr[ index].type = "food"){ totalPrice = (totalPrice + ( arr[ index ].price * arr[ index ].quantity )*1.2)}
+
+      //else {(totalPrice = totalPrice + ( arr[ index ].price * arr[ index ].quantity )};
+   
+    //}
+    
+    // 4. Return the totalPrice Variable
+    //return totalPrice.toFixed( 2 );
+    
+  //}
+  
+  //document.write( '<div class="total-cost alert alert-primary" role="alert">£ ' + shoppingCartTotalPrice( shoppingCart ) + '</div>') ; // 5. Console.log the returned value -  I replaced with document.write()
   
